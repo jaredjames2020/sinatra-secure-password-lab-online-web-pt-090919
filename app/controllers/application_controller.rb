7acd02884e5e662013bada7ccd9c117a922da9e3
Jared S <jaredjames2020@gmail.com>
2019-12-18 01:28:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
     user = User.new(username: params[:username], password: params[:password])
     binding.pry
     if user
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect to '/account'
     elsif params[:username].empty? || params[:password].empty?
       redirect to '/failure'
